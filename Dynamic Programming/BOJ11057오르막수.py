834bd8e4b1eabce82495cabe7e32e6a5c7ead508
da2inee <105406914+da2inee@users.noreply.github.com>
2023-02-15 23:34:10 +0900

    Update BOJ11057오르막수.py

--- Dynamic Programming/BOJ11057오르막수.py
+++ Dynamic Programming/BOJ11057오르막수.py
@@ -1,7 +1,12 @@
-from itertools import combinations
+#dp 문제는 항상 규칙을 찾으려고 노력하자!!
 N = int(input())
-num = []
-for comb in combinations(range(0,10),N):
-    comb = list(comb)
-    num.append(comb)
-print(len(num))
+dp = [[1]*10 for _ in range(N)]
+
+for i in range(1,N):
+    for j in range(1,10):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+ans = 0
+for i in range(10):
+    ans += dp[N-1][i]
+
+print(ans%10007)
