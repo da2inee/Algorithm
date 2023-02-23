cfe0efb8cd886d6223e00e5531916466398e8642
da2inee <105406914+da2inee@users.noreply.github.com>
2023-02-24 03:03:13 +0900

    update dynamic programming

--- Dynamic Programming/BOJ1149RGB거리.py
+++ Dynamic Programming/BOJ1149RGB거리.py
@@ -1,12 +1,13 @@
 N = int(input())
-home = []
+world = []
 for i in range(N):
-    home.append(list(map(int,input().split())))
-dp = [0,0,0]
-dp[0] = home[0][0]
-dp[1] = home[0][1]
-dp[2] = home[0][2] 
-a = 0
+    world.append(list(map(int,input().split())))
+dp = [[0]*3 for _ in range(N)]
+dp[0][0] = world[0][0]
+dp[0][1] = world[0][1]
+dp[0][2] = world[0][2]
 for i in range(1,N):
-    if a == 0:
-        dp[] 
+    dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + world[i][0]
+    dp[i][1] = min(dp[i-1][0],dp[i-1][2]) + world[i][1]
+    dp[i][2] = min(dp[i-1][1],dp[i-1][0]) + world[i][2]
+print(min(dp[-1]))
